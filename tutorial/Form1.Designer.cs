dfae34349007c92a44b8761388d9ada02a0c44bd
GODOFMINECRAFT4 <xgodofminecraftx@gmail.com>
2017-05-11 19:57:47 +1200

    GitHub Push

---
+++ tutorial/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace tutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit_button = new System.Windows.Forms.Button();
+            this.main_text = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username_box = new System.Windows.Forms.TextBox();
+            this.password_box = new System.Windows.Forms.TextBox();
+            this.DebugButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // submit_button
+            // 
+            this.submit_button.Location = new System.Drawing.Point(105, 99);
+            this.submit_button.Name = "submit_button";
+            this.submit_button.Size = new System.Drawing.Size(75, 23);
+            this.submit_button.TabIndex = 0;
+            this.submit_button.Text = "Submit";
+            this.submit_button.UseVisualStyleBackColor = true;
+            this.submit_button.Click += new System.EventHandler(this.submit_button_Click);
+            // 
+            // main_text
+            // 
+            this.main_text.AutoSize = true;
+            this.main_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.main_text.Location = new System.Drawing.Point(71, 9);
+            this.main_text.Name = "main_text";
+            this.main_text.Size = new System.Drawing.Size(158, 13);
+            this.main_text.TabIndex = 1;
+            this.main_text.Text = "Please type your details to login.";
+            this.main_text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.main_text.Click += new System.EventHandler(this.main_text_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // username_box
+            // 
+            this.username_box.Location = new System.Drawing.Point(74, 37);
+            this.username_box.Name = "username_box";
+            this.username_box.Size = new System.Drawing.Size(198, 20);
+            this.username_box.TabIndex = 4;
+            this.username_box.Tag = "YouTube";
+            // 
+            // password_box
+            // 
+            this.password_box.Location = new System.Drawing.Point(74, 68);
+            this.password_box.Name = "password_box";
+            this.password_box.PasswordChar = '*';
+            this.password_box.Size = new System.Drawing.Size(198, 20);
+            this.password_box.TabIndex = 5;
+            this.password_box.Tag = "Videos_2_@";
+            // 
+            // DebugButton
+            // 
+            this.DebugButton.Location = new System.Drawing.Point(197, 99);
+            this.DebugButton.Name = "DebugButton";
+            this.DebugButton.Size = new System.Drawing.Size(75, 23);
+            this.DebugButton.TabIndex = 6;
+            this.DebugButton.Text = "Debug ";
+            this.DebugButton.UseVisualStyleBackColor = true;
+            this.DebugButton.Click += new System.EventHandler(this.DebugButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 134);
+            this.Controls.Add(this.DebugButton);
+            this.Controls.Add(this.password_box);
+            this.Controls.Add(this.username_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.main_text);
+            this.Controls.Add(this.submit_button);
+            this.Name = "Form1";
+            this.Text = "Login Menu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit_button;
+        private System.Windows.Forms.Label main_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username_box;
+        private System.Windows.Forms.TextBox password_box;
+        private System.Windows.Forms.Button DebugButton;
+    }
+}
+
---
+++ tutorial/Game.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace tutorial
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.Player = new System.Windows.Forms.Label();
+            this.Life = new System.Windows.Forms.ProgressBar();
+            this.Enemy_01 = new System.Windows.Forms.PictureBox();
+            this.Pear_02 = new System.Windows.Forms.PictureBox();
+            this.Banana_02 = new System.Windows.Forms.PictureBox();
+            this.Apple_02 = new System.Windows.Forms.PictureBox();
+            this.Pear_01 = new System.Windows.Forms.PictureBox();
+            this.Banana_01 = new System.Windows.Forms.PictureBox();
+            this.Apple_01 = new System.Windows.Forms.PictureBox();
+            this.Inv_05 = new System.Windows.Forms.PictureBox();
+            this.Inv_03 = new System.Windows.Forms.PictureBox();
+            this.Inv_04 = new System.Windows.Forms.PictureBox();
+            this.Inv_02 = new System.Windows.Forms.PictureBox();
+            this.Inv_01 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemDebug = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemDebugInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemDebugInfo_2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy_01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pear_02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Banana_02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Apple_02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pear_01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Banana_01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Apple_01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_05)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_04)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_01)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.AutoSize = true;
+            this.Player.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player.Location = new System.Drawing.Point(376, 326);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(98, 25);
+            this.Player.TabIndex = 0;
+            this.Player.Text = "Character";
+            // 
+            // Life
+            // 
+            this.Life.Location = new System.Drawing.Point(0, 27);
+            this.Life.Name = "Life";
+            this.Life.Size = new System.Drawing.Size(183, 23);
+            this.Life.TabIndex = 1;
+            // 
+            // Enemy_01
+            // 
+            this.Enemy_01.Image = global::tutorial.Properties.Resources.Enemy_01;
+            this.Enemy_01.Location = new System.Drawing.Point(600, 119);
+            this.Enemy_01.Name = "Enemy_01";
+            this.Enemy_01.Size = new System.Drawing.Size(32, 32);
+            this.Enemy_01.TabIndex = 13;
+            this.Enemy_01.TabStop = false;
+            this.Enemy_01.Tag = "Enemy";
+            // 
+            // Pear_02
+            // 
+            this.Pear_02.Image = global::tutorial.Properties.Resources.untitled_jpg;
+            this.Pear_02.Location = new System.Drawing.Point(796, 523);
+            this.Pear_02.Name = "Pear_02";
+            this.Pear_02.Size = new System.Drawing.Size(32, 32);
+            this.Pear_02.TabIndex = 12;
+            this.Pear_02.TabStop = false;
+            this.Pear_02.Tag = "Pear";
+            // 
+            // Banana_02
+            // 
+            this.Banana_02.Image = global::tutorial.Properties.Resources.banana_jpg;
+            this.Banana_02.Location = new System.Drawing.Point(85, 214);
+            this.Banana_02.Name = "Banana_02";
+            this.Banana_02.Size = new System.Drawing.Size(32, 32);
+            this.Banana_02.TabIndex = 11;
+            this.Banana_02.TabStop = false;
+            this.Banana_02.Tag = "Banana";
+            // 
+            // Apple_02
+            // 
+            this.Apple_02.Image = global::tutorial.Properties.Resources.Apple;
+            this.Apple_02.Location = new System.Drawing.Point(771, 174);
+            this.Apple_02.Name = "Apple_02";
+            this.Apple_02.Size = new System.Drawing.Size(32, 32);
+            this.Apple_02.TabIndex = 10;
+            this.Apple_02.TabStop = false;
+            this.Apple_02.Tag = "Apple";
+            // 
+            // Pear_01
+            // 
+            this.Pear_01.Image = global::tutorial.Properties.Resources.untitled_jpg;
+            this.Pear_01.Location = new System.Drawing.Point(368, 105);
+            this.Pear_01.Name = "Pear_01";
+            this.Pear_01.Size = new System.Drawing.Size(32, 32);
+            this.Pear_01.TabIndex = 9;
+            this.Pear_01.TabStop = false;
+            this.Pear_01.Tag = "Pear";
+            // 
+            // Banana_01
+            // 
+            this.Banana_01.Image = global::tutorial.Properties.Resources.banana_jpg;
+            this.Banana_01.Location = new System.Drawing.Point(428, 557);
+            this.Banana_01.Name = "Banana_01";
+            this.Banana_01.Size = new System.Drawing.Size(32, 32);
+            this.Banana_01.TabIndex = 8;
+            this.Banana_01.TabStop = false;
+            this.Banana_01.Tag = "Banana";
+            // 
+            // Apple_01
+            // 
+            this.Apple_01.Image = global::tutorial.Properties.Resources.Apple;
+            this.Apple_01.Location = new System.Drawing.Point(104, 591);
+            this.Apple_01.Name = "Apple_01";
+            this.Apple_01.Size = new System.Drawing.Size(32, 32);
+            this.Apple_01.TabIndex = 7;
+            this.Apple_01.TabStop = false;
+            this.Apple_01.Tag = "Apple";
+            // 
+            // Inv_05
+            // 
+            this.Inv_05.Image = ((System.Drawing.Image)(resources.GetObject("Inv_05.Image")));
+            this.Inv_05.Location = new System.Drawing.Point(812, 12);
+            this.Inv_05.Name = "Inv_05";
+            this.Inv_05.Size = new System.Drawing.Size(32, 32);
+            this.Inv_05.TabIndex = 6;
+            this.Inv_05.TabStop = false;
+            this.Inv_05.Click += new System.EventHandler(this.Inv_05_CLick);
+            // 
+            // Inv_03
+            // 
+            this.Inv_03.Image = ((System.Drawing.Image)(resources.GetObject("Inv_03.Image")));
+            this.Inv_03.Location = new System.Drawing.Point(888, 12);
+            this.Inv_03.Name = "Inv_03";
+            this.Inv_03.Size = new System.Drawing.Size(32, 32);
+            this.Inv_03.TabIndex = 5;
+            this.Inv_03.TabStop = false;
+            this.Inv_03.Click += new System.EventHandler(this.Inv_03_CLick);
+            // 
+            // Inv_04
+            // 
+            this.Inv_04.Image = global::tutorial.Properties.Resources.Blank_Inv;
+            this.Inv_04.Location = new System.Drawing.Point(850, 12);
+            this.Inv_04.Name = "Inv_04";
+            this.Inv_04.Size = new System.Drawing.Size(32, 32);
+            this.Inv_04.TabIndex = 4;
+            this.Inv_04.TabStop = false;
+            this.Inv_04.Click += new System.EventHandler(this.Inv_04_CLick);
+            // 
+            // Inv_02
+            // 
+            this.Inv_02.Image = ((System.Drawing.Image)(resources.GetObject("Inv_02.Image")));
+            this.Inv_02.Location = new System.Drawing.Point(926, 12);
+            this.Inv_02.Name = "Inv_02";
+            this.Inv_02.Size = new System.Drawing.Size(32, 32);
+            this.Inv_02.TabIndex = 3;
+            this.Inv_02.TabStop = false;
+            this.Inv_02.Click += new System.EventHandler(this.Inv_02_CLick);
+            // 
+            // Inv_01
+            // 
+            this.Inv_01.Image = ((System.Drawing.Image)(resources.GetObject("Inv_01.Image")));
+            this.Inv_01.Location = new System.Drawing.Point(964, 12);
+            this.Inv_01.Name = "Inv_01";
+            this.Inv_01.Size = new System.Drawing.Size(32, 32);
+            this.Inv_01.TabIndex = 2;
+            this.Inv_01.TabStop = false;
+            this.Inv_01.Click += new System.EventHandler(this.Inv_01_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.MaximumSize = new System.Drawing.Size(250, 25);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(250, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemDebug,
+            this.toolStripSeparator1,
+            this.MenuItemExit});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.toolStripMenuItem1.Text = "Options";
+            // 
+            // MenuItemDebug
+            // 
+            this.MenuItemDebug.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemDebugInfo,
+            this.MenuItemDebugInfo_2});
+            this.MenuItemDebug.Name = "MenuItemDebug";
+            this.MenuItemDebug.Size = new System.Drawing.Size(152, 22);
+            this.MenuItemDebug.Text = "Debug";
+            // 
+            // MenuItemDebugInfo
+            // 
+            this.MenuItemDebugInfo.Name = "MenuItemDebugInfo";
+            this.MenuItemDebugInfo.Size = new System.Drawing.Size(207, 22);
+            this.MenuItemDebugInfo.Text = "Print Debug Info Console";
+            this.MenuItemDebugInfo.Click += new System.EventHandler(this.MenuItemDebugInfo_Click);
+            // 
+            // MenuItemDebugInfo_2
+            // 
+            this.MenuItemDebugInfo_2.Name = "MenuItemDebugInfo_2";
+            this.MenuItemDebugInfo_2.Size = new System.Drawing.Size(207, 22);
+            this.MenuItemDebugInfo_2.Text = "Debug Info MessageBox";
+            this.MenuItemDebugInfo_2.Click += new System.EventHandler(this.MenuItemDebugInfo_2_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // MenuItemExit
+            // 
+            this.MenuItemExit.Name = "MenuItemExit";
+            this.MenuItemExit.Size = new System.Drawing.Size(152, 22);
+            this.MenuItemExit.Text = "Exit";
+            this.MenuItemExit.Click += new System.EventHandler(this.MenuItemExit_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.Enemy_01);
+            this.Controls.Add(this.Pear_02);
+            this.Controls.Add(this.Banana_02);
+            this.Controls.Add(this.Apple_02);
+            this.Controls.Add(this.Pear_01);
+            this.Controls.Add(this.Banana_01);
+            this.Controls.Add(this.Apple_01);
+            this.Controls.Add(this.Inv_05);
+            this.Controls.Add(this.Inv_03);
+            this.Controls.Add(this.Inv_04);
+            this.Controls.Add(this.Inv_02);
+            this.Controls.Add(this.Inv_01);
+            this.Controls.Add(this.Life);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Game";
+            this.Text = " Game";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Enemy_01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pear_02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Banana_02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Apple_02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pear_01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Banana_01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Apple_01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_05)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_04)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Inv_01)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Player;
+        private System.Windows.Forms.ProgressBar Life;
+        private System.Windows.Forms.PictureBox Inv_01;
+        private System.Windows.Forms.PictureBox Inv_02;
+        private System.Windows.Forms.PictureBox Inv_04;
+        private System.Windows.Forms.PictureBox Inv_03;
+        private System.Windows.Forms.PictureBox Inv_05;
+        private System.Windows.Forms.PictureBox Apple_01;
+        private System.Windows.Forms.PictureBox Banana_01;
+        private System.Windows.Forms.PictureBox Pear_01;
+        private System.Windows.Forms.PictureBox Apple_02;
+        private System.Windows.Forms.PictureBox Banana_02;
+        private System.Windows.Forms.PictureBox Pear_02;
+        private System.Windows.Forms.PictureBox Enemy_01;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemDebug;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemDebugInfo;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemDebugInfo_2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemExit;
+    }
+}
